e5212b84f4005c3ffe7245eb608e95da33d82bbc
Logan Donley <ldonley@cloudbees.com>
2020-03-19 13:15:12 -0400

    Adding slack invitation

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask, render_template, redirect
 from python_freeipa import ClientMeta
+import requests
+import logging
 
 from forms import SignupForm
 
@@ -11,6 +13,8 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "ookeemo7ohGhahx9da9l
 client = ClientMeta(os.environ.get("LDAP_SERVER"), verify_ssl=False)
 client.login(os.environ.get("LDAP_ADMIN"), os.environ.get("LDAP_PASSWORD"))
 
+logger = logging.getLogger('registration')
+
 
 @app.route("/", methods=["GET", "POST"])
 def registration():
@@ -18,7 +22,7 @@ def registration():
     if form.validate_on_submit():
         print(form.email)
         full_name = f"{form.first_name.data} {form.last_name.data}"
-        user = client.user_add(
+        client.user_add(
             a_uid=form.username.data,
             o_givenname=form.first_name.data,
             o_sn=form.last_name.data,
@@ -26,7 +30,16 @@ def registration():
             o_userpassword=form.password.data,
             o_preferredlanguage="EN",
         )
-        print(user)
+        if os.environ.get("SLACK_ENABLED"):
+            try:
+                data = {
+                    "email": form.email.data,
+                    "token": os.environ.get("SLACK_API_TOKEN"),
+                    "set_active": "true"
+                }
+                requests.post(f"https://{os.environ.get('SLACK_WORKSPACE')}.slack.com/api/users.admin.invite", data=data)
+            except Exception as e:
+                logger.error(f"Challenges sending slack invite to {form.email.data} - {e}")
         return redirect("/success")
     return render_template("register.html", form=form)
 
