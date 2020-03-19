6995315b6d4ac9be6897b75a0e06501aa7d02e29
Logan Donley <ldonley@cloudbees.com>
2020-03-19 11:03:36 -0400

    Got basic form working

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from forms import SignupForm
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'ookeemo7ohGhahx9da9l'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def registration():
-    return render_template("register.html")
+    form = SignupForm()
+    if form.validate_on_submit():
+        print(form.email)
+        return redirect('/success')
+    return render_template("register.html", form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template("success.html")
 
 
 if __name__ == '__main__':
